38a86ffbadc4439e0ffd1c2718e68c7b3c0bc1c6
AlkoMoon <alkomoon2012>
2018-08-09 11:13:20 +0300

    Initial commit

---
+++ android/src/com/unit/ft_bird/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.unit.ft_bird;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.unit.ft_bird.FtBird;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FtBird(), config);
+	}
+}
